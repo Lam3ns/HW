5b49af8ef061a24b8254b5e38d9b4567e3639117
kalte <lamensakk@gmail.com>
2024-01-30 18:39:37 +0200

    first

---
+++ HW.py
@@ -0,0 +1,17 @@
+#3 Задание
+# a = ("Сніг іде, а я сиджу вдома з чашкою гарячого чаю і читаю книгу. ")
+# b = a.count(".") + a.count("!") + a.count("?")
+# print(b)
+
+
+#1 Задание
+# a = input(" ")
+# if a == a[::-1]:
+#     print("Паліндром ")
+
+#2 Задание
+# a = input( )
+# b = input("Список зарезервованих слів ").split()
+# c = " ".join(w.upper() if w.lower() in b else w for w in a.split())
+# print("Змінений текст ")
+# print(c)
